47aeb32de0be1749846c7fe1f22b71bbd73c4c08
rafaelpeinado <rafael.peinado.silva@gmail.com>
2025-10-29 11:00:03 -0300

    chore: variaveis de ambiente

--- src/db.ts
+++ src/db.ts
@@ -4,11 +4,11 @@ import { DatabaseResult } from './types';
 dotenv.config();
 
 const pool = new Pool({
-    user: process.env.DB_USER || 'appuser',
-    password: process.env.DB_PASSWORD || 'appsecret',
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
     host: process.env.DB_HOST || 'localhost',
     port: parseInt(process.env.DB_PORT || '5432'),
-    database: process.env.DB_NAME || 'certo_ou_errado'
+    database: process.env.DB_NAME
 });
 
 pool.on('connect', () => {
